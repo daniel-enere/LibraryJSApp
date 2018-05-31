dd27c13774ce49739a8462990b9a3f823036e789
daniel-enere <daniel.enere@gmail.com>
2018-05-30 21:34:32 -0500

    added sign in page and authorization

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({secret: 'lib'}));
+
+require('./src/config/passport.js')(app);
+
 
 // app.use((req, res, next) => {
 //   debug('my middleware');
@@ -26,9 +37,12 @@ const nav = [
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 const adminRouter = require('./src/routes/adminRoutes')(nav);
+const authRouter = require('./src/routes/authRoutes')(nav);
+
 
 app.use('/books', bookRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, '/views/index.html')); //first iteration
